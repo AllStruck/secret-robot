05c0bfd35e08fdb4ccd54714b6d05bbc4e6877e7
David Monaghan <github@allstruck.com>
2013-06-24 00:45:13 -0700

    Added basic openstack commands and starting on replacing server side scripts...

--- fabfile.py
+++ fabfile.py
@@ -1,56 +1,60 @@
+#!/usr/bin/python
 from __future__ import with_statement
+from IPython import embed
 from fabric.api import *
 from fabric.contrib.console import confirm
+from fabric.colors import red, green, blue, yellow
+import json
+import os
+import openstack.compute
+import clouddns
+import datetime
+
+now = datetime.datetime.now()
+datetime = now.strftime("%Y-%m-%d_%H:%M")
 
 from cuisine import *
 
 with open('./keys.json') as f:
-	env = json.load(f)
+	keychain = json.load(f)
+	compute = openstack.compute.Compute(username=keychain['rackspaceuser'], apikey=keychain['rackspaceapikey'])
+	dns = clouddns.connection.Connection(keychain['rackspaceuser'], keychain['rackspaceapikey'])
 
-import clouddns
-dns = clouddns.connection.Connection(env.rackspace-user, env.rackspace-api-key)
+with open('./host-aliases.json') as f:
+	hostaliases = json.load(f)
+	for key,val in enumerate(env.hosts):
+		env.hosts[key] = nicenametohost(val)
 
-env.warn_only = True
-env.hosts = ['allstruck.org:42123']
 env.user = 'root'
 
 vhostroot = '/var/www/vhost/'
+apachevhostconfigdir = '/etc/apache2/sites-available'
 
 def setup():
 	group_ensure("remote_admin")
 	user_ensure("admin")
 	group_user_ensure("remote_admin", "admin")
 
+def addtohosts(host):
+	env.hosts.append(str(host))
+	env.host_string = str(host)
+	updatehosts()
+
+def updatehosts():
+	for key,val in enumerate(env.hosts):
+		env.hosts[key] = nicenametohost(val)
+		env.host_string = nicenametohost(val)
+
 def dovhostcommand(vhost, command):
 	location = vhostroot + vhost + 'public/'
 	if "wwwpermissions" in command:
 		wwwpermissions(location)
 
-def splitsubdomain(domain):
-	entry = parent = domain
-	if domain.count('.') > 1:
-		entry = domain
-		parent = domain[domain.rfind('.',0,domain.rfind('.'))+1:]
-	return entry,parent
-
-def gethostip(host):
-	ipaddresses = {
-		'allstruck.com': 		'198.101.193.150',
-		'allstruck.net': 		'204.232.207.253',
-		'allstruck.org:42123': 	'204.232.201.73',
-		'cooper':				'50.56.239.226'
-	}
-	return ipaddresses[host]
-
-def createrandompassword(length=8):
-	import string
-	from random import sample, choice
-	chars = string.letters + string.digits
-	return ''.join(choice(chars) for _ in xrange(length))
-
-def newlampvhost(domain, dbname, installcms=False, ip=False):
+def newlampvhost(domain, dbname, installcms=False, ip=False, skipbackup=False):
 	if not ip:
-		ip = gethostip(env.host_string)
+		ip = gethostipbyhost(env.host_string)
+	if not skipbackup:
+		serverimagemk(True)
 	entry,parent = splitsubdomain(domain)
 	dnsmk(parent,ip,entry)
 	newapachevhost(domain)
@@ -63,7 +67,9 @@ def newlampvhost(domain, dbname, installcms=False, ip=False):
 		wwwpermissions(vhostroot + domain + 'public/')
 
 def newapachevhost(url):
-	run('a2newsite ' + url)
+	with cd(apachevhostconfigdir):
+		put('./apache-vhost-template.conf', url)
+	#run('a2newsite ' + url)
 
 def newmysqldb(dbname):
 	run('sqlnewdb ' + dbname)
@@ -101,6 +107,13 @@ def dnsmk(domain, location=False, subdomain=False, recordtype='A'):
 			print "Creating root domain " + domain
 			dns.create_domain(name=domain, ttl=300, emailAddress=domain + '@allstruck.com')
 
+def aptupdate():
+	run('sudo apt-get update')
+
+def aptupgrade():
+	run('sudo apt-get upgrade')
+
+# RackSpace Server and DNS Stuff:
 @hosts('allstruck.org')
 def dnsrm(domain, subdomain=False):
 	try:
@@ -126,8 +139,172 @@ def dnsls(domain=False):
 		for domain in dns.get_domains():
 			print domain.name
 
-def sysupdate():
-	run('sudo apt-get update')
+def serverimagemk(wait=False):
+	server = compute.servers.find(name=getserverbyname(env.host_string))
+	compute.images.create(getserverbyname(env.host_string) + '-' + datetime, server)
+	image = compute.images.find(name=getserverbyname(env.host_string) + '-' + datetime)
+	if wait:
+		print "Waiting for server image to complete."
+		serverimageprogresswatcher(image.id)
 
-def sysupgrade():
-	run('sudo apt-get upgrade')
+def serverimagels(imagename=False,imageid=False):
+	if imagename or imageid:
+		if imageid:
+			image = compute.images.get(imageid)
+			print str(image) + ' {imageid=' + str(image.id) + '} ' + serverimagestatus(image.status)
+			if image.status == 'SAVING':
+				print 'Progress: ' + str(image.progress) + '%'
+			print 'Created: ' + str(image.created)
+			print 'Updated: ' + str(image.updated)
+		else:
+			images = compute.images.list()
+			for image in images:
+				if imagename in image.name:
+					print str(image) + ' {imageid=' + str(image.id) + '} ' + serverimagestatus(image.status)
+					if image.status == 'SAVING':
+						print 'Progress: ' + str(image.progress) + '%'
+					print 'Created: ' + str(image.created)
+					print 'Updated: ' + str(image.updated)
+					print '---'
+	else:
+		images = compute.images.list()
+		for image in images:
+			print str(image) + ' {imageid=' + str(image.id) + '} ' + serverimagestatus(image.status)
+			if image.status == 'SAVING':
+				print str(image.progress) + '%'
+
+def serverimagestatus(status):
+	if status == 'ACTIVE':
+		return green(status)
+	elif status == 'QUEUED':
+		return yellow(status)
+	elif status == 'SAVING':
+		return blue(status)
+	else:
+		return red(status)
+
+def serverimageprogress(imageid=False, verbosity=1):
+	if not imageid:
+		print "ERROR: Must provide imageid=id"
+		return -1
+	if not server_image_complete(imageid, 0):
+		image = compute.images.get(imageid)
+		if hasattr(image, 'status'):
+			if image.status == 'SAVING':
+				if hasattr(image, 'progress'):
+					if verbosity > 0:
+						print 'Progress: ' + str(image.progress) + '%'
+						return image.progress
+				else:
+					return 0
+			elif image.status == 'QUEUED':
+				if verbosity > 0:
+					print 'Queued'
+				return 0
+			else:
+				 if verbosity > 0:
+				 	print '100% Complete'
+		else:
+			if verbosity > 0:
+				print "There's not a status property on image :("
+			return 0
+	else:
+		if verbosity > 0:
+			print "100 % Complete"
+		return 100
+
+def server_image_complete(imageid=False, verbosity=1):
+	if not imageid:
+		print "ERROR: Must provide imageid=id"
+		return
+	image = compute.images.get(imageid)
+	if image.status == 'SAVING':
+		if verbosity > 0:
+			print 'Not done.'
+		return False
+	elif image.status == 'ACTIVE':
+		if verbosity > 0:
+			print 'Complete!'
+		return True
+	elif image.status == 'QUEUED':
+		if verbosity > 0:
+			print 'You entered the Queue, this is going to suck...'
+		return False
+	else:
+		print 'Problem! image.status is: ' + str(image.status)
+		return
+
+def serverimageprogresswatcher(imageid, delay=23, verbosity=0):
+	import time
+	if not imageid:
+		print "ERROR: Must provide imageid=id"
+		return
+	while not server_image_complete(imageid, verbosity):
+		print str(serverimageprogress(imageid, verbosity)) + '%'
+		time.sleep(delay)
+
+
+def serverls(instance=False):
+	if instance:
+		servers = compute.servers.findall(name=getserverbyname(instance))
+		for server in servers:
+			print 'Name: ' + server.name
+	else:
+		servers = compute.servers.list();
+		for server in servers:
+			print server
+			details = compute.servers.get(server)
+			print 'Status: ' + details.status
+			if details.status == 'ACTIVE':
+				print 'Addresses: ' + str(details.addresses)
+			else:
+				print 'Progress: ' + str(details.progress)
+			print ''
+
+def splitsubdomain(domain):
+	entry = parent = domain
+	if domain.count('.') > 1:
+		entry = domain
+		parent = domain[domain.rfind('.',0,domain.rfind('.'))+1:]
+	return entry,parent
+
+def gethostipbyhost(host):
+	ipaddresses = {
+		'allstruck.com': 		'198.101.193.150',
+		'allstruck.net:42123':	'204.232.207.253',
+		'allstruck.org:42123': 	'204.232.201.73',
+		'50.56.239.226:42123':	'50.56.239.226'
+	}
+	return ipaddresses[host]
+
+def getserverbyname(host):
+	names = {
+		'allstruck.com': 		'Archimedes',
+		'allstruck.net:42123': 	'AllStruckClients',
+		'allstruck.org:42123': 	'McKay',
+		'50.56.239.226:42123':	'Cooper'
+	}
+	if host in names:
+		if names[host]:
+			return names[host]
+		else:
+			return names[names.index(host)]
+
+def nicenametohost(nicename):
+	if nicename in hostaliases:
+		return hostaliases[nicename]
+
+def createrandompassword(length=8):
+	import string
+	from random import sample, choice
+	chars = string.letters + string.digits
+	return ''.join(choice(chars) for _ in xrange(length))
+
+def test(message):
+	print message
+
+def stest(message):
+	run('echo ' + message)
+
+def interactive():
+	embed() # this call anywhere in your program will start IPython
